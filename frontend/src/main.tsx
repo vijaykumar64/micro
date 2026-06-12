f4f823a07f868d4cc4154e10e4192b63107a977d
vinay2902H <subscriptions3.ssl@gmail.com>
2026-06-12 18:55:13 +0530

    chore: force frontend rebuild with VITE_API_URL

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// build: force rebuild with VITE_API_URL
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
